ef65537d4c8ee528c38acef56f663f9c0631c08b
= <=>
2025-10-05 19:31:13 -0400

    Added Pauli X Gate.

--- gates.py
+++ gates.py
@@ -1,11 +1,35 @@
 import numpy as np
 
+ZERO = np.array((1, 0))
+ONE = np.array((0, 1))
+
 class Gate:
-    def __init__(self, name, matrix):
+    def __init__(self, name, matrix: np.array):
         self.name = name
         self.matrix = matrix
 
     def __repr__(self):
-        return f"Gate(name={self.name}, matrix={self.matrix})"
+        return f"Gate(name = {self.name}, matrix = {self.matrix})"
+    
+    def __len__(self):
+        return len(self.matrix)
+    
+    @property
+    def T(self):
+        return self.matrix.T
+    
+    def conj(self): 
+        return self.matrix.conj()
+    
+    def __mul__(self, other):
+        return self.matrix @ other
 
-class       
+class PauliX(Gate):
+    def __init__(self, name):
+        print(np.column_stack((ZERO, ONE)))
+        super().__init__(name, np.column_stack((ZERO, ONE)))
+    
+    
+    
+    
+    
--- qubit.py
+++ qubit.py
@@ -1,5 +1,8 @@
 import random
 import math
+import cmath
+import numpy as np
+from gates import Gate, PauliX
 
 class Qubit:
 
@@ -7,22 +10,24 @@ class Qubit:
         self.theta = math.pi*random.random()
         self.phi = 2*math.pi*random.random()
         self.zero_ket = math.cos(self.theta/2)
-        self.one_ket = math.sin(self.theta/2)
-        self.rel_phase = math.e**self.phi # e^(i*phi)
+        self.one_ket = cmath.exp(1j * self.phi) * math.sin(self.theta/2)
     
     def __repr__(self):
-        if self.one_ket < 0:
-            return f"Quantum State: {self.zero_ket}|0> + ({self.one_ket}|1>)"
-        return f"Quantum State: {self.zero_ket}|0> + {self.one_ket}|1>"
+        return f"Quantum State: {self.zero_ket}|0> + ({self.one_ket}|1>)"
 
     def measure_std(self):
-        val = random.random()
-        if val < self.zero_ket**2:
+        if random.random() < abs(self.zero_ket)**2:
             return 0
-        else: return 1
+        return 1
 
     def get_state(self):
-        return (self.zero_ket, self.one_ket)
+        return np.array([self.zero_ket, self.one_ket], dtype=complex)
+    
+    def operate(self, g: Gate):
+        assert np.allclose(np.eye(len(g)), g*g.T.conj())
+        new_state = g*self.get_state()
+        self.zero_ket = new_state[0]
+        self.one_ket = new_state[1]
     
 
 if __name__ == "__main__":
@@ -31,12 +36,19 @@ if __name__ == "__main__":
     qubit = Qubit()
     print(qubit)
     tot = 0
+    gate = PauliX("px")
 
     for i in range(100):
         qubit = Qubit()
-        norm = sum(x**2 for x in qubit.get_state())
+        norm = sum(abs(x)**2 for x in qubit.get_state())
+        q_zero = qubit.get_state()[0]
+        q_one = qubit.get_state()[1]
         assert (norm < 1 + tol and norm > 1 - tol), f"{norm}"
         if qubit.measure_std() == 1:
             tot += 1
+        
+        qubit.operate(gate)
+        assert (q_zero == qubit.zero_ket and q_one == qubit.one_ket)
+
 
     print(tot)
